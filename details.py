47d081109913fc704179bc1579f14f6d1b4d39d1
MrSaiCodes <saiwebsite4us@gmail.com>
2025-04-17 15:37:49 +0530

    Update details.py

--- details.py
+++ details.py
@@ -4,8 +4,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", 23454876))
 API_HASH = getenv("API_HASH", "b42626ee535fcaf915232af564a95bea")
-BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:8027492776:AAFfBBiQpzp-xLDHSRkLaz0HygBhQyyrgKE")
-OWNER_ID = int(getenv("OWNER_ID", "8043047247"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8043047247").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
+BOT_TOKEN = getenv("BOT_TOKEN", "7705829246:AAGkrOC-1RiFyhqndeuva45RIbUGs54HfRI")
+OWNER_ID = int(getenv("OWNER_ID", "662494886"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "662494886").split()))
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://saik:F@v@NHpt@7TFAb9@cluster0.brflswq.mongodb.net/?retryWrites=true&w=majority")
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002253133527"))
